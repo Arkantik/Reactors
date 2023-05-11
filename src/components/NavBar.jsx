7b9b080966bd684801440300eb859f9bb87eee99
Denis Dezest <denis.dezest@gmail.com>
2023-05-12 00:17:56 +0200

    Updated NavBar logo and active NavLinks color

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,57 +2,62 @@ import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 export default function Navbar() {
-	return (
-		<header>
-			<nav className="px-4">
-				<NavLink to="/">
-					<img
-						src="../public/assets/images/logo.png"
-						alt="logo HoliFun"
-						width="50"
-						height="50"
-						className="logo-desktop"
-					/>
-				</NavLink>
-				<ul className="flex gap-16 pr-4 font-bold no-underline">
-					<li className="no-underline">
-						<NavLink
-							to="/"
-							className={({ isActive }) =>
-								`navlink ${isActive ? "is-active" : ""}`
-							}>
-							Home
-						</NavLink>
-					</li>
-					<li>
-						<NavLink
-							to="/destinations"
-							className={({ isActive }) =>
-								`navlink ${isActive ? "is-active" : ""}`
-							}>
-							Destinations
-						</NavLink>
-					</li>
-					<li>
-						<NavLink
-							to="/contact"
-							className={({ isActive }) =>
-								`navlink ${isActive ? "is-active" : ""}`
-							}>
-							Contact
-						</NavLink>
-					</li>
-					<li>
-						<NavLink
-							to="/about"
-							className={({ isActive }) =>
-								`navlink ${isActive ? "is-active" : ""}`
-							}>
-							About
-						</NavLink>
-					</li>
-				</ul>
-			</nav>
-		</header>
-	);
+  return (
+    <header>
+      <nav className="px-4">
+        <NavLink to="/">
+          <img
+            // src="../public/assets/images/logo.png"
+            src="../public/assets/images/logo-2.png"
+            alt="logo HoliFun"
+            width="48"
+            height="48"
+            className="logo-desktop"
+          />
+        </NavLink>
+        <ul className="flex gap-16 pr-4 font-bold no-underline">
+          <li className="no-underline">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                `navlink ${isActive ? "is-active" : ""}`
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/destinations"
+              className={({ isActive }) =>
+                `navlink ${isActive ? "is-active" : ""}`
+              }
+            >
+              Destinations
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/contact"
+              className={({ isActive }) =>
+                `navlink ${isActive ? "is-active" : ""}`
+              }
+            >
+              Contact
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="/about"
+              className={({ isActive }) =>
+                `navlink ${isActive ? "is-active" : ""}`
+              }
+            >
+              About
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
