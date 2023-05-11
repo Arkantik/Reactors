21b10423de01dd5817987f6ec7242082538301c1
Hafsa <hafsa.aitouarraou@outlook.fr>
2023-05-11 22:13:38 +0200

    card list done & changed first pic

--- src/pages/Destinations.jsx
+++ src/pages/Destinations.jsx
@@ -5,20 +5,22 @@ import { useEffect } from "react";
 
 export default function Destinations() {
   const destinations = JSON.parse(localStorage.getItem("destinations"));
-
   return (
-    <section>
+    <section className="mb-10">
       <h1 className="mb-2 flex justify-center">Destinations</h1>
       <div className="flex gap-4 justify-center items-center mb-2">
         <SearchBar />
         <FilterBtn />
       </div>
-      <ul className=" flex flex-wrap justify-center gap-8">
-        {[...Array(7)].map((destination, index) => (
+
+      <ul className="flex flex-wrap justify-center gap-8">
+        {destinations.map((destination, index) =>{
+          return (
           <li key={index}>
-            <Card />
+            <Card destination={destination} />
           </li>
-        ))}
+          )
+        })}
       </ul>
     </section>
   );
