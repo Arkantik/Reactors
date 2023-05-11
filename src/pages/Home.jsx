1ae44ac49eaa9da0a66743c1e1293e3491235656
Denis Dezest <denis.dezest@gmail.com>
2023-05-11 15:25:15 +0200

    Updated structure of data.json

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,22 @@
-// import { useState } from "react";
+import { useState } from "react";
 import useFetch from "../helpers/useFetch";
+import infoDestinations from "../data/data";
 
 export default function Home() {
   const API = import.meta.env.VITE_API;
-  // const [destinations, setDestinations] = useState([]);
+  const [destinations, setDestinations] = useState([]);
 
   // 1. fetch data from API (custom hook)
   const { data, loading } = useFetch(API + "/all");
   if (!loading) console.log(data);
 
   // 2. filter data to retain only desired destinations
+  console.log(infoDestinations);
+
+  //get country name
+  console.log(infoDestinations[0].name.common);
+  //get city name
+  console.log(infoDestinations[0].cities[0].cityName);
 
   return (
     <section>
