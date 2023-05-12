a2ba689567afc0aa29809f2ddcbd31d1fc46d95e
Arkantik <95821033+Arkantik@users.noreply.github.com>
2023-05-12 11:25:56 +0200

    Completion of about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -3,23 +3,22 @@ import DevCard from "../components/DevCard";
 import devTeam from "../data/devTeam.json";
 
 export default function About() {
-  const developers = devTeam;
+	const developers = devTeam;
 
-  return (
-    <section className="h-full bg-[url('/assets/images/background-about.png')] bg-cover bg-no-repeat px-4">
-      <div className="grid h-full grid-cols-1 grid-rows-[auto_1fr] overflow-y-auto">
-        <h1 className="text-center">The Reactors Team 🚀 </h1>
-        <ul className="mb- flex flex-wrap justify-center gap-[3vw] self-center">
-          {developers.map((developer) => (
-            <li
-              key={developer.id}
-              className="flex h-fit w-[200px] max-w-[250px] flex-col items-center gap-4 overflow-hidden rounded-lg bg-neutral-50/30 pb-4 drop-shadow-xl backdrop-blur-md hover:animate-bounce md:w-[250px]"
-            >
-              <DevCard {...developer} />
-            </li>
-          ))}
-        </ul>
-      </div>
-    </section>
-  );
+	return (
+		<section className="h-full bg-[url('../assets/images/background-about.png')] bg-cover bg-no-repeat px-4">
+			<div className="grid h-full grid-cols-1 grid-rows-[auto_1fr] overflow-y-auto">
+				<h1 className="text-center">The Reactors Team 🚀 </h1>
+				<ul className="mb- flex flex-wrap justify-center gap-[3vw] self-center">
+					{developers.map((developer) => (
+						<li
+							key={developer.id}
+							className="flex h-fit w-[200px] max-w-[250px] flex-col items-center gap-4 overflow-hidden rounded-lg bg-neutral-50/30 pb-4 drop-shadow-xl backdrop-blur-md md:w-[250px]">
+							<DevCard {...developer} />
+						</li>
+					))}
+				</ul>
+			</div>
+		</section>
+	);
 }
