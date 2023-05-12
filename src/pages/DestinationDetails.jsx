c417d8d0d742141428146a2bf0e267df69031ea5
Hafsa <hafsa.aitouarraou@outlook.fr>
2023-05-12 14:06:48 +0200

    ajout de sauts de lignes

--- src/pages/DestinationDetails.jsx
+++ src/pages/DestinationDetails.jsx
@@ -10,54 +10,61 @@ export default function DestinationDetails() {
     const filteredDestination = destinations.filter(
       (destination) => destination.id == id
     );
-    console.log(filteredDestination)
+    console.log(filteredDestination);
     setDestination(filteredDestination[0]);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
-    <section className="h-full px-4 overflow-y-auto grid grid-cols-1 auto-rows-min md:grid-cols-3 md:gap-4">
+    <section className="grid h-full auto-rows-min grid-cols-1 overflow-y-auto px-4 md:grid-cols-3 md:gap-4">
       <h1 className="text-center md:col-span-3">
         {destination.cities ? destination.cities[0].name : null}
       </h1>
 
-      <div className="md:col-span-2 grid grid-cols-2 gap-4">
+      <div className="grid grid-cols-2 gap-4 md:col-span-2">
         <img
           src={destination.cities ? destination.cities[0].picture.image1 : null}
           alt={destination.cities ? destination.cities[0].name + " 1" : null}
-          className="w-full max-h-[360px] h-40 md:h-[30vw] object-cover rounded-lg"
+          className="h-40 max-h-[360px] w-full rounded-lg object-cover md:h-[30vw]"
         />
         <img
           src={destination.cities ? destination.cities[0].picture.image2 : null}
           alt={destination.cities ? destination.cities[0].name + " 2" : null}
-          className="w-full max-h-[360px] h-40 md:h-[30vw] object-cover  rounded-lg"
+          className="h-40 max-h-[360px] w-full rounded-lg object-cover  md:h-[30vw]"
         />
         <img
           src={destination.cities ? destination.cities[0].picture.image3 : null}
           alt={destination.cities ? destination.cities[0].name + " 3" : null}
-          className="w-full max-h-[360px] h-40 md:h-[30vw] object-cover  rounded-lg"
+          className="h-40 max-h-[360px] w-full rounded-lg object-cover  md:h-[30vw]"
         />
         <img
           src={destination.cities ? destination.cities[0].picture.image4 : null}
           alt={destination.cities ? destination.cities[0].name + " 4" : null}
-          className="w-full max-h-[360px] h-40 md:h-[30vw] object-cover  rounded-lg"
+          className="h-40 max-h-[360px] w-full rounded-lg object-cover  md:h-[30vw]"
         />
       </div>
 
-      <article className="mt-4 md:mt-0 md:row-span-4 md:border-neutral-300 md:border md:p-4 md:rounded-lg md:shadow-lg">
-        <h2 className="font-bold mb-2">
+      <article className="mt-4 md:row-span-4 md:mt-0 md:rounded-lg md:border md:border-neutral-300 md:p-4 md:shadow-lg">
+        <h2 className="mb-2 font-bold">
           {destination.name ? destination.name.common : null}
         </h2>
-        <img src={destination.cities ? destination.cities[0].flag : null} alt="flag" className="mb-8"/>
-        <p className="text-gray-700">
-          {destination.cities ? destination.cities[0].description : null}
-        </p>{" "}
-        <p className="text-gray-700 mt-4">
-        </p>
+        <img
+          src={destination.cities ? destination.cities[0].flag : null}
+          alt="flag"
+          className="mb-8"
+        />
+        <p
+          className="text-gray-700"
+          dangerouslySetInnerHTML={{
+            __html: destination.cities
+              ? destination.cities[0].description
+              : null,
+          }}
+        />
       </article>
 
       <article className="mt-4 md:col-span-2">
-        <h2 className="font-bold mb-2">
+        <h2 className="mb-2 font-bold">
           🎭 Things To Do In{" "}
           {destination.cities ? destination.cities[0].name : null}
         </h2>
@@ -75,7 +82,7 @@ export default function DestinationDetails() {
       </article>
 
       <article className="my-4 md:col-span-2">
-        <h2 className="font-bold mb-2">🛎️ Restaurants</h2>
+        <h2 className="mb-2 font-bold">🛎️ Restaurants</h2>
         <ul className="list-disc pl-4">
           <li>
             {destination.cities
